03fdd89247a4fc5b8a6a2da2ee7eb183cd2cacf6
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-03-01 10:18:12 +0100

    save

--- src/json/molleweide.json.rb
+++ src/json/molleweide.json.rb
@@ -13,14 +13,20 @@ def main
         'description' => 'molleweide personal',
         'manipulators' => [
 
+          # todo
+          #
+          # upper left corner ESCAPE always get back to
+          # base layer/state. Eg. if turn of all keyr escape
+          # would bring you back/turn on keys..
+
           ##########################################
           # LAYER -> BASE (use no variable? hmm..) #
           ##########################################
 
-          # thumbs
-          # Layers.generate_dual_key_rule('spacebar', 'spacebar', 'left_shift'),
-          Layers.generate_dual_key_rule('left_command', 'tab', 'left_command'),
-          Layers.generate_dual_key_rule('right_command', 'return_or_enter', 'right_command'),
+          # sides
+          Layers.generate_single_key_rule('caps_lock', 'escape'),
+          Layers.generate_single_key_rule('left_shift', 'delete_or_backspace'),
+          Layers.generate_single_key_rule('right_shift', 'delete_or_backspace'),
 
           # homerow dual keys
           Layers.generate_dual_key_rule('a', 'a', 'left_control'),
@@ -32,30 +38,38 @@ def main
           Layers.generate_dual_key_rule('l', 'l', 'right_shift'),
           Layers.generate_dual_key_rule('semicolon', 'semicolon', 'right_control'),
 
-          # remappings
-          Layers.generate_single_key_rule('caps_lock', 'escape'), # cntrl work around
-          # Layers.generate_single_key_rule('left_command', 'tab'),
-          # Layers.generate_single_key_rule('right_command', 'return_or_enter'),
-          Layers.generate_single_key_rule('left_shift', 'delete_or_backspace'),
-          Layers.generate_single_key_rule('right_shift', 'delete_or_backspace'),
-
-          #####################################################
-          # LAYER -> 'BASE_SPACE' | hold spacebar to activate #
-          #####################################################
-
+          # thumbs
           Layers.STATE_HOLD_ACTIVATE('STATE_HOLD_SPACE', 'spacebar', 'spacebar'),
+          Layers.generate_dual_key_rule('left_command', 'tab', 'left_command'),
+          Layers.generate_dual_key_rule('right_command', 'return_or_enter', 'right_command'),
 
-          # homerow left
+          ###########################################################
+          # LAYER -> 'STATE_HOLD_SPACE' | hold spacebar to activate #
+          ###########################################################
+
+          # homerow
           Layers.DUAL_KEY('STATE_HOLD_SPACE', 'a', 'escape', 'left_control'),
           Layers.DUAL_KEY('STATE_HOLD_SPACE', 's', 'tab', 'left_shift'),
           Layers.DUAL_KEY('STATE_HOLD_SPACE', 'd', 'return_or_enter', 'left_option'),
           Layers.DUAL_KEY('STATE_HOLD_SPACE', 'f', 'delete_or_backspace', 'left_command'),
-
-          # vi arrows
+          # g
           Layers.SINGLE_KEY('STATE_HOLD_SPACE', 'h', 'left_arrow'),
           Layers.SINGLE_KEY('STATE_HOLD_SPACE', 'j', 'down_arrow'),
           Layers.SINGLE_KEY('STATE_HOLD_SPACE', 'k', 'up_arrow'),
           Layers.SINGLE_KEY('STATE_HOLD_SPACE', 'l', 'right_arrow'),
+          # ;
+
+          #############################################
+          # LAYER -> 'STATE_SYMBOLS_AND_NUMPAD' | todo #
+          #############################################
+
+          # put symbols left hand
+          #
+          # numpad on right hand
+
+          #############################################
+          # LAYER -> 'STATE_TURN_OFF_ALL_KEYS' | todo #
+          #############################################
 
         ],
       },
