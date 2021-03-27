d9e7b9708f159631deb1cfe090cab148ed03ed73
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-03-27 18:05:47 +0100

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require("gulp");
+const run = require("gulp-run-command").default;
+
+// const ROOT_DIR = "./"; // get file dir??
+
+
+// const WATCH_DIR = "./src/json/mollew****?";
+
+
+// 1. build profile
+// 2. cp into ~/.config/karabiner/a
+// const BUILD_CMD = `make -C ${ROOT_DIR} ergodox_ez:myergo`;
+
+
+// gulp.task(
+//   "build",
+//   gulp.series(
+//     run(BUILD_CMD, {
+//       ignoreErrors: true,
+//     })
+//   )
+// );
+
+
+// gulp.task(
+//   "watch",
+//   gulp.series("build", () => {
+//     gulp.watch(WATCH_DIR, gulp.series("build"));
+//   })
+// );
+
+
+// gulp.task("default", gulp.series("watch"));
