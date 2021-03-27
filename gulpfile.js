e7b3bf3d88340c5b6161874ce20131c5a877f4a8
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-03-27 18:18:33 +0100

    setup watch. how do i watch regex files

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,39 @@
 const gulp = require("gulp");
 const run = require("gulp-run-command").default;
 
-// const ROOT_DIR = "./"; // get file dir??
+// const ROOT_DIR = "./"; // unnecessary since we run in root
 
-
+// watch layout files
 // const WATCH_DIR = "./src/json/mollew****?";
 
 
-// 1. build profile
-// 2. cp into ~/.config/karabiner/a
-// const BUILD_CMD = `make -C ${ROOT_DIR} ergodox_ez:myergo`;
+// const BUILD_CMD          = `make`;
+
+// copy based on bulk regex mollew**
+// const COPY_MOLLEW_CMD    = `cp public/json/molleweide.json ~/.config/karabiner/assets/complex_modifications`;
+// const COPY_MOLL_NDK_CMD  = `cp public/json/molleweide_ndk.json ~/.config/karabiner/assets/complex_modifications`;
+
+gulp.task("copy_moll", run(COPY_MOLLEW_CMD));
+gulp.task("copy_moll_ndk", run(COPY_MOLL_NDK_CMD));
+
+ gulp.task(
+   "build",
+   gulp.series(
+     run(BUILD_CMD, {
+       ignoreErrors: true,
+     }),
+     "copy_moll",
+     "copy_moll_ndk",
+   )
+ );
 
 
-// gulp.task(
-//   "build",
-//   gulp.series(
-//     run(BUILD_CMD, {
-//       ignoreErrors: true,
-//     })
-//   )
-// );
+gulp.task(
+  "watch",
+  gulp.series("build", () => {
+    gulp.watch(WATCH_DIR, gulp.series("build"));
+  })
+);
 
 
-// gulp.task(
-//   "watch",
-//   gulp.series("build", () => {
-//     gulp.watch(WATCH_DIR, gulp.series("build"));
-//   })
-// );
-
-
-// gulp.task("default", gulp.series("watch"));
+gulp.task("default", gulp.series("watch"));
